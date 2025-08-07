06625603418a2554c43cd62bed75b7211b9144dc
Samara Cassie <76058965+samaraCassie@users.noreply.github.com>
2025-08-07 13:11:26 -0300

    initial

---
+++ src/main/java/talent_flow/talent_flow/TalentFLowApplication.java
@@ -0,0 +1,13 @@
+package talent_flow.talent_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentFLowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalentFLowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/talent_flow/talent_flow/TalentFLowApplicationTests.java
@@ -0,0 +1,13 @@
+package talent_flow.talent_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentFLowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
